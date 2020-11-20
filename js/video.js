941cfd32d3720f7b6f245bf1ac66ce287643d27e
Michael Cooke <michael.frank.cooke@gmail.com>
2020-11-20 13:13:30 -0500

    Add updateVolumeSpan()

--- js/video.js
+++ js/video.js
@@ -1,15 +1,23 @@
 let video = document.querySelector('video')
 const volumeSpan = document.getElementById('volume')
 
+function updateVolumeSpan () {
+  volumeSpan.innerHTML = video.volume * 100 + '%'
+}
+
 window.addEventListener('load', function () {
   video = document.querySelector('#myVideo')
 })
 
 document.querySelector('#play').addEventListener('click', function () {
   video.play()
-  volumeSpan.innerHTML = video.volume * 100 + '%'
+  updateVolumeSpan()
 })
 
 document.querySelector('#pause').addEventListener('click', function () {
   video.pause()
+})
+
+document.querySelector('#volumeSlider').addEventListener('change', function () {
+  video.volume = this.value / 100
 })
