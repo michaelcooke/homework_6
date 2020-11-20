c95f4944b8a719ca41e23d08e908dd3945e9a575
Michael Cooke <michael.frank.cooke@gmail.com>
2020-11-20 13:14:37 -0500

    Revert "Add updateVolumeSpan()"
    This reverts commit 941cfd32d3720f7b6f245bf1ac66ce287643d27e.

--- js/video.js
+++ js/video.js
@@ -1,23 +1,15 @@
 let video = document.querySelector('video')
 const volumeSpan = document.getElementById('volume')
 
-function updateVolumeSpan () {
-  volumeSpan.innerHTML = video.volume * 100 + '%'
-}
-
 window.addEventListener('load', function () {
   video = document.querySelector('#myVideo')
 })
 
 document.querySelector('#play').addEventListener('click', function () {
   video.play()
-  updateVolumeSpan()
+  volumeSpan.innerHTML = video.volume * 100 + '%'
 })
 
 document.querySelector('#pause').addEventListener('click', function () {
   video.pause()
-})
-
-document.querySelector('#volumeSlider').addEventListener('change', function () {
-  video.volume = this.value / 100
 })
